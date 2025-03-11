ac6f6c3077e0b50a4cce9be58a84c1cd9af5ccf2
Allain Legaspi <alegaspi_ccs@uspf.edu.ph>
2025-03-11 18:20:13 +0800

    Fixed Bugs
    fixed user auth errors

---
+++ Library_Management/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2025 Library Information Management System | Designed by : Allain Ralph Legaspi</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
